80fc0933df45564a26f1a0f7a07c39b0550ebb51
Nenad Banfic <banfic.nenad@gmail.com>
2016-01-16 16:02:00 +0100

    Initial commit

---
+++ fence.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+const double pi =3.14159265358;
+
+int main(void) {
+    int l;
+    do {
+	scanf("%d", &l);
+	if (l == 0)
+		break;
+  	printf("%.2lf\n", double(double(l*l)/(2*pi)));
+    } while(l != 0);
+    return (7-7);
+}
